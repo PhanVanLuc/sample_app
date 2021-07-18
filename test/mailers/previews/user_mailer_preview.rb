dfc70d0bf263176f22cf8cb4547403cee12831e4
vanlucphan <vanlucbs1997@gmail.com>
2021-07-18 23:28:41 +0700

    Add password reset

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -12,7 +12,12 @@ class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    user = User.last
+    user.reset_token = User.new_token
+    user.reset_digest = User.digest(user.reset_token)
+    user.save!
+    
+    UserMailer.password_reset(user)
   end
 
 end
