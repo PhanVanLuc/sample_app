b8744113cc87e60d4bfe989f951db06f71d45025
vanlucphan <vanlucbs1997@gmail.com>
2021-07-21 09:26:00 +0700

    add i18n

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     rescue
-      flash[:success] = "User not found"
+      flash[:success] = t('users.new.flash_show')
       redirect_to root_url
   end
   def new
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = t('users.create.flash_create')
       redirect_to @user
     else
     render 'new'
@@ -19,6 +19,6 @@ class UsersController < ApplicationController
   end
   private
   def user_params
-    params.require(:user).permit(:name, :email, :password,:password_confirmation) 
+    params.require(:user).permit(:name, :email, :password, :password_confirmation) 
   end
 end
