a0ec4dc23be7fd60616cc45c79adf03ac3f0a221
vanlucphan <vanlucbs1997@gmail.com>
2021-07-21 21:51:00 +0700

    Add i18n

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,17 @@ Rails.application.routes.draw do
   get 'password_resets/new'
   get 'password_resets/edit'
   get 'sessions/new'
-  resources :users
-  get 'users/new'
-  root 'static_pages#home'
-  get '/help', to: 'static_pages#help'
-  get '/about', to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
-  get '/signup', to: 'users#new'
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  scope "(:locale)", locale: /en|vi/ do
+    get 'users/new'
+    root 'static_pages#home'
+    get '/help', to: 'static_pages#help'
+    get '/about', to: 'static_pages#about'
+    get '/contact', to: 'static_pages#contact'
+    get '/signup', to: 'users#new'
+    get '/login', to: 'sessions#new'
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
+  end
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
